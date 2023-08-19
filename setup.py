9c0bb7c6c1f06b3ca50d46c6ed7500e1e01edab2
Gautham Nair <gautham.p.nair@gmail.com>
2023-08-19 14:21:36 -0400

    move all into setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,14 @@ from Cython.Build import cythonize
 compiled = cythonize("src/foo/bar.pyx")
 
 setup(
-    ext_modules=compiled
+    name="foo",
+    version="0.0.1",
+    url="https://github.com/gauthamnair/pants-exmaple-local-dist",
+    author="Gautham Nair",
+    author_email="gautham@example.com",
+    ext_modules=compiled,
+    package_dir={"": "src"},
+    python_requires=">=3.9",
+    install_requires=["tabulate"],
+    packages=["foo"],
 )
